5d597dd7c0c4fc2cc0a5aedb4f2fd378051606e6
Moham-Ghanem <mohammedghanem91@gmail.com>
2020-05-17 02:05:25 +0200

    First plot

---
+++ Working/first_plot.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 00:24:11 2020
+
+@author: Ghanem
+"""
+
+import numpy 
+import matplotlib.pyplot as pyplot 
+
+x = numpy.linspace(0,5,20)    
+print(x)  
+y = x**2    
+
+pyplot.plot(x,y)           
+pyplot.show()              
+
